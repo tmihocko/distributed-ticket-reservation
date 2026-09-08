4c6d12aa24b1a071bc44cf3be3f147b77e2ffe89
tmihock <trmcburger@gmail.com>
2026-09-07 20:43:22 -0400

    Initial

---
+++ src/main/java/com/mihocko/jsp/JspApplication.java
@@ -0,0 +1,13 @@
+package com.mihocko.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspApplication.class, args);
+	}
+
+}
